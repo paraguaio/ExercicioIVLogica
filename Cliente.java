def02632c8d27a5262d79cc4e2b00b6e18867aad
Ícaro Nogueira <79952012+paraguaio@users.noreply.github.com>
2021-03-13 15:41:05 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,28 @@
+
+package exercicioivlogica;
+
+
+public class Cliente 
+        
+{
+    
+    public String nome;
+    private int preco;
+    
+    public String getNome() 
+    {
+        return nome;
+    }
+    
+    public int getPreco() 
+    {
+        return preco;
+    }
+
+    public void setPreco (int preco) 
+    {
+        this.preco = preco;
+    }
+
+    
+}
