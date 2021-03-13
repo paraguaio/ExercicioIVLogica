68fe2b93d5efd84963829f5fc8e5ad3b53da4cfa
Ícaro Nogueira <79952012+paraguaio@users.noreply.github.com>
2021-03-13 15:41:29 -0300

    Create Livro.java

---
+++ Livro.java
@@ -0,0 +1,27 @@
+
+package exercicioivlogica;
+
+
+
+public class Livro 
+{   
+    
+    public String livro;
+    private String genero;
+
+   public String getLivro() 
+    {
+        return livro;
+    }
+    
+    public String getGenero() 
+    {
+        return genero;
+    }
+
+    public void setGenero (String genero) 
+    {
+        this.genero = genero;
+    }
+
+}
